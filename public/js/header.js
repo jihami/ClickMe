5b69ac9f29bf01370d9a5bc1b87ab30972d99d20
jihami <w2027@e-mirim.hs.kr>
2022-06-14 06:49:05 +0900

    header 텍스트 변경, 경고 수정

--- public/js/header.js
+++ public/js/header.js
@@ -1,10 +1,3 @@
-const colorData= {
-    'pRandom' : 'timeAuto',
-    'rRandom' :'random',
-    'pRGadient' : 'timeGradient',
-    'hexcode' : '#B897FF',
-    'clickMe' : '#713fe8'
-}
 function pop(id){
     $('#popup').show()
     console.log(id)
@@ -15,31 +8,31 @@ function color(colorCode){
     console.log(pId)
     switch (colorCode){
         case "pR":
-            cC = "timeAuto"; break;
+            colorCode = "timeAuto"; break;
         case "rR":
-            cC = "random"; break;
+            colorCode = "random"; break;
         case "gR":
-            cC = "timeGradient"; break;
+            colorCode = "timeGradient"; break;
         case "H":
-            cC = "B897FF"; break;
+            colorCode = "B897FF"; break;
         case "C":
-            cC = "713fe8"; break;
+            colorCode = "713fe8"; break;
         default:
-            cC = "713fe8";
+            colorCode = "713fe8";
     }
     const headerData = {
-        'Wave'  : '<img src="https://capsule-render.vercel.app/api?type=wave&color='+cC+'&height=200&text=WAVE" style="width:100%" style="width:100%"/>',
-        'Egg'  : '<img src="https://capsule-render.vercel.app/api?type=egg&color='+cC+'&height=210" style="width:100%"/>',
-        'Shark'  : '<img src="https://capsule-render.vercel.app/api?type=shark&color='+cC+'&height=140" style="width:100%"/>',
-        'Slice'  : '<img src="https://capsule-render.vercel.app/api?type=slice&color='+cC+'&height=200&text=SLICE&fontAlign=70&rotate=13&fontAlignY=25&desc=desc%20function%20is%20also%20rotated.&descAlign=70.&descAlignY=44" style="width:100%"/>',
-        'React'  : '<img src="https://capsule-render.vercel.app/api?type=rect&color='+cC+'&text=%20%20RECT%20%20&fontAlign=30&fontSize=30&textBg=true&desc=Use%20%27textBg%27%20to%20highlight%20%27text%27&descAlign=60&descAlignY=50" style="width:100%"/>',
-        'Soft'  : '<img src="https://capsule-render.vercel.app/api?type=soft&color='+cC+'&text=Good%20to%20use%20with%20other%20readme&fontSize=40&animation=twinkling" style="width:100%"/>',
-        'Rounded'  : '<img src="https://capsule-render.vercel.app/api?type=rounded&color='+cC+'&text=Rounded%20with%20stroke&fontAlignY=50&fontSize=40&height=200&stroke=000000&strokeWidth=2" style="width:100%"/>',
-        'Cylinder'  : '<img src="https://capsule-render.vercel.app/api?type=cylinder&color='+cC+'&text=Cylinder&fontAlignY=45&fontSize=40&height=150&animation=blinking&desc=desc%20is%20also%20animated&descAlignY=70" style="width:100%"/>',
-        'Waving'  : '<img src="https://capsule-render.vercel.app/api?type=waving&height=200&text=Waving!&fontAlign=80&fontAlignY=40&color='+cC+'" style="width:100%"/>',
-        'Transparent'  : '<img src="https://capsule-render.vercel.app/api?type=transparent&fontColor='+cC+'&text=Transparent&height=150&fontSize=60&desc=Only%20Use%20Text&descAlignY=75&descAlign=60" style="width:100%"/>'
+        'Wave'  : '<img src="https://capsule-render.vercel.app/api?type=wave&color='+colorCode+'&height=200&text=ClickMe" style="width:100%" style="width:100%" alt="HEADER"/>',
+        'Egg'  : '<img src="https://capsule-render.vercel.app/api?type=egg&color='+colorCode+'&height=210" style="width:100%"  alt="HEADER"/>',
+        'Shark'  : '<img src="https://capsule-render.vercel.app/api?type=shark&color='+colorCode+'&height=140" style="width:100%" alt="HEADER"/>',
+        'Slice'  : '<img src="https://capsule-render.vercel.app/api?type=slice&color='+colorCode+'&height=200&text=ClickMe&fontAlign=70&rotate=13&fontAlignY=25&desc=desc%20function%20is%20also%20rotated.&descAlign=70.&descAlignY=44" style="width:100%" alt="HEADER"/>',
+        'React'  : '<img src="https://capsule-render.vercel.app/api?type=rect&color='+colorCode+'&text=ClickMe&fontAlign=30&fontSize=30&textBg=true&desc=Use%20%27textBg%27%20to%20highlight%20%27text%27&descAlign=60&descAlignY=50" style="width:100%" alt="HEADER"/>',
+        'Soft'  : '<img src="https://capsule-render.vercel.app/api?type=soft&color='+colorCode+'&text=ClickMe&fontSize=40&animation=twinkling" style="width:100%" alt="HEADER"/>',
+        'Rounded'  : '<img src="https://capsule-render.vercel.app/api?type=rounded&color='+colorCode+'&text=ClickMe&fontAlignY=50&fontSize=40&height=200&stroke=000000&strokeWidth=2" style="width:100%" alt="HEADER"/>',
+        'Cylinder'  : '<img src="https://capsule-render.vercel.app/api?type=cylinder&color='+colorCode+'&text=ClickMe&fontAlignY=45&fontSize=40&height=150&animation=blinking&desc=desc%20is%20also%20animated&descAlignY=70" style="width:100%" alt="HEADER"/>',
+        'Waving'  : '<img src="https://capsule-render.vercel.app/api?type=waving&height=200&text=Waving!&fontAlign=80&fontAlignY=40&color='+colorCode+'" style="width:100%" alt="HEADER"/>',
+        'Transparent'  : '<img src="https://capsule-render.vercel.app/api?type=transparent&fontColor='+colorCode+'&text=ClickMe&height=150&fontSize=60&desc=Only%20Use%20Text&descAlignY=75&descAlign=60" style="width:100%" alt="HEADER"/>'
     }
-    txt = headerData[pId]
+    let txt = headerData[pId]
     console.log(txt)
     document.getElementById('headerText').innerHTML = txt;
     document.getElementById('headerExample').innerHTML = txt;
@@ -102,10 +95,7 @@ function githubLogOut() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         document.getElementById("user_name").innerHTML = "Git Hub Login"
-    }).catch((error) => {
-        // An error happened.
-        console.log("logoutError")
-    });
+    })
 
     // 로그아웃 시 버튼 숨기기
     $(function(){
@@ -118,7 +108,7 @@ function githubLogOut() {
 
 // 세션이 있다면 로그인 상태 유지
 $(function (){
-    if(sessionStorage.length != 0){
+    if(sessionStorage.length !== 0){
         document.getElementById('user_name').innerHTML = sessionStorage.getItem("name")+"님"
 
         $(function(){
